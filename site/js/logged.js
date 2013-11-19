1e1d54cf310de979ef6f4e017dd6c1d98d40bd38
Bousky <nicolas.bouscarle@centraliens-lille.org>
2013-11-19 14:14:21 +0100

    Affichage de base

--- site/js/logged.js
+++ site/js/logged.js
@@ -39,3 +39,45 @@ function init() {
 				);
 }
 
+function drawPlateau(dataPlatau) {
+    var plateau = document.getElementById("plateau");
+    var height = dataPlateau.length;
+    var width = dataPlateau[0].length;
+    for (var y = 0 ; y < height ; y++) {
+        var row = plateau.insertRow(-1);
+        for (var x= 0 ; x < width ; x++) {
+            var cell = row.insertCell(-1);
+            cell.style.border = "1px grey dotted";
+            cell.style.width = "30px";
+            cell.style.height = "30px";
+            if (dataPlateau[y][x].g)
+                cell.style.borderLeft = "2px black solid";
+            if (dataPlateau[y][x].d)
+                cell.style.borderRight = "2px black solid";
+            if (dataPlateau[y][x].h)
+                cell.style.borderTop = "2px black solid";
+            if (dataPlateau[y][x].b)
+                cell.style.borderBottom = "2px black solid";
+            cell.onmousedown = function(){beginDrag(x, y)};
+            cell.onmouseover = function(){hoverDrag(x, y)};
+            cell.onmouseout = function(){hoverDragOut(x, y)};
+        }
+    }
+}
+
+function getCell(x, y) {
+    return document.getElementById("plateau").rows[y].cells[x];
+}
+
+// drawRobot(x, y, color) affiche un robot de couleur "color" dans la case (x,y) 
+// drawRobot(x, y)        efface un robot dans la case (x,y)
+function drawRobot(x, y, color) {
+    if (color === undefined)
+        color = "";
+    var cell = getCell(x, y);
+    cell.style.backgroundColor = color;
+    if (color === "")
+        cell.onmousedown = undefined;
+    else
+        cell.onmousedown = function(){beginDrag(x, y)};
+}
