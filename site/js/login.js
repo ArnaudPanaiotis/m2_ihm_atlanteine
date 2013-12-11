d7e90e721bf432401ca9be86c09698646c18e15a
Bousky <nicolas.bouscarle@centraliens-lille.org>
2013-12-11 09:52:04 +0100

    clic sur la liste des parties pour connexioin rapide

--- site/js/login.js
+++ site/js/login.js
@@ -2,16 +2,22 @@ function init() {
 	// Connect to the SocketIO server to retrieve ongoing games.
 	socket = io.connect();
 	socket.on('gamesList', function(data) {
-								 var ul = document.getElementById('lesParties');
-								 ul.innerHTML='';
-								 for(p in data.gamesList) {
-									 var li = document.createElement('li'); 
-									 ul.appendChild( li );
-									 li.appendChild( document.createTextNode( data.gamesList[p] ) );
-									}
-								}
-			 );
+                var ul = document.getElementById('lesParties');
+                ul.innerHTML='';
+                for(var p in data.gamesList) {
+                    var li = document.createElement('li'); 
+                    ul.appendChild( li );
+                    li.innerHTML = "<a href='javascript:void(0);' onclick=\"startGame('"+data.gamesList[p]+"')\">"+data.gamesList[p]+"</a>";
+                    //li.appendChild( document.createTextNode( data.gamesList[p] ) );
+                }
+            }
+        );
 	socket.emit('loginPage');
+}
+
+function startGame(gameId) {
+    document.getElementById("idGame").value = gameId;
+    document.getElementById("nouvellePartie").submit();
 }
 
 
